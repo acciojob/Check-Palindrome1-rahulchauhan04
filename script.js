c38ee5c63c61335b52fb9741274687fbac6e852c
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-02-10 20:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// complete the given function
+function palindrome(str) {
+  // Remove all non-alphanumeric characters (punctuation, spaces, and symbols) and turn everything into the same case (lowercase or uppercase) to check for palindromes.
+  var cleanStr = str.replace(/[\W_]/g, '').toLowerCase();
 
-function palindrome(str){
-
+  // Compare the cleaned string with its reverse.
+  var reverseStr = cleanStr.split('').reverse().join('');
+  
+  // Check if the reversed string is strictly equal to the cleaned string.
+  return cleanStr === reverseStr;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
